d3413c92df8ceaaa44c305986af43ced7a7d1824
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-04-09 20:09:26 +0530

    feat: add moment.js for date handling in flight creation

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
+const moment = require('moment');
 
 const { FlightRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
@@ -8,9 +9,26 @@ const flightRepository = new FlightRepository();
 
 const createFlight = async (data) => {
   try {
+    const arrivalTime = moment(data.arrivalTime, 'YYYY-MM-DD HH:mm:ss');
+    const departureTime = moment(data.departureTime, 'YYYY-MM-DD HH:mm:ss');
+    if (
+      arrivalTime.isBefore(departureTime) ||
+      arrivalTime.isSame(departureTime)
+    ) {
+      throw new AppError(
+        ['Departure time should be less than the arrival time.'],
+        StatusCodes.BAD_REQUEST
+      );
+    }
     const flight = await flightRepository.create(data);
     return flight;
   } catch (error) {
+    if (error.statusCode === StatusCodes.BAD_REQUEST) {
+      throw new AppError(
+        ['Departure time should be less than the arrival time.'],
+        StatusCodes.BAD_REQUEST
+      );
+    }
     if (
       error.name === 'SequelizeUniqueConstraintError' ||
       error.name === 'SequelizeValidationError'
